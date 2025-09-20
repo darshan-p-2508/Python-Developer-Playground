3c5bf408246ff31fbb50d9739b5fa4519d7a9331
Darshan Prashanth <140430203+darshan-p-2508@users.noreply.github.com>
2025-09-20 16:43:54 +0530

    Created hangman_words.py

---
+++ Basics/hangman/hangman_words.py
@@ -0,0 +1,14 @@
+word_list = [
+    "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+    "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
+    "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "xigua", "yam", "zucchini",
+    "acorn", "bamboo", "cactus", "daisy", "elm", "fern", "gardenia", "hibiscus",
+    "ivy", "jasmine", "kelp", "lavender", "maple", "nutmeg", "oak", "palm",
+    "quiver", "rose", "sage", "tulip", "umbrella", "violet", "willow", "xerophyte",
+    "yarrow", "zephyr", "antelope", "buffalo", "catfish", "dolphin", "elephant", "falcon",
+    "giraffe", "hyena", "iguana", "jaguar", "kangaroo", "lemur", "mongoose", "narwhal",
+    "octopus", "penguin", "quokka", "rabbit", "seal", "tortoise", "uakari", "vulture",
+    "wolf", "xerus", "yak", "zebra", "apricot", "blueberry", "cranberry", "dragonfruit",
+    "elderflower", "feijoa", "grapefruit", "hazelnut", "indigo", "jackfruit", "kumquat", "lime",
+    "mulberry", "nectar", "olive", "pineapple", "quinoa", "rhubarb", "spinach"
+]
