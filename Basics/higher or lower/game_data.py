a3c59927153016cd4f6b932af3fb0212b1365e78
Darshan Prashanth <140430203+darshan-p-2508@users.noreply.github.com>
2025-09-25 23:44:41 +0530

    Create game_data.py

---
+++ Basics/higher or lower/game_data.py
@@ -0,0 +1,290 @@
+data = [
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 630,
+        "description": "Footballer, sports icon, fitness",
+        "country": "Portugal"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 500,
+        "description": "Footballer, world cup winner",
+        "country": "Argentina"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 400,
+        "description": "Model, influencer, beauty mogul",
+        "country": "USA"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 395,
+        "description": "Actor, wrestler, fitness inspiration",
+        "country": "USA"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 429,
+        "description": "Singer, actress, mental health",
+        "country": "USA"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 380,
+        "description": "Singer, actress, pop star",
+        "country": "USA"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 360,
+        "description": "Celebrity, influencer, entrepreneur",
+        "country": "USA"
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 275,
+        "description": "Cricketer, fitness, sports icon",
+        "country": "India"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 290,
+        "description": "Singer, songwriter, global star",
+        "country": "USA"
+    },
+    {
+        "name": "Nicki Minaj",
+        "follower_count": 240,
+        "description": "Rapper, singer, pop culture",
+        "country": "Trinidad"
+    },
+    {
+        "name": "Rihanna",
+        "follower_count": 270,
+        "description": "Singer, fashion, beauty mogul",
+        "country": "Barbados"
+    },
+    {
+        "name": "Shah Rukh Khan",
+        "follower_count": 42,
+        "description": "Actor, Bollywood superstar",
+        "country": "India"
+    },
+    {
+        "name": "Elon Musk",
+        "follower_count": 185,
+        "description": "Entrepreneur, innovator, SpaceX Tesla",
+        "country": "USA"
+    },
+    {
+        "name": "Jeff Bezos",
+        "follower_count": 15,
+        "description": "Entrepreneur, Amazon founder billionaire",
+        "country": "USA"
+    },
+    {
+        "name": "Oprah Winfrey",
+        "follower_count": 50,
+        "description": "Host, philanthropist, media icon",
+        "country": "USA"
+    },
+    {
+        "name": "Billie Eilish",
+        "follower_count": 120,
+        "description": "Singer, songwriter, Grammy winner",
+        "country": "USA"
+    },
+    {
+        "name": "Zendaya",
+        "follower_count": 180,
+        "description": "Actress, model, singer, influencer",
+        "country": "USA"
+    },
+    {
+        "name": "Drake",
+        "follower_count": 145,
+        "description": "Rapper, singer, global superstar",
+        "country": "Canada"
+    },
+    {
+        "name": "LeBron James",
+        "follower_count": 160,
+        "description": "Basketball, NBA legend athlete",
+        "country": "USA"
+    },
+    {
+        "name": "Neymar Jr",
+        "follower_count": 300,
+        "description": "Footballer, Brazilian sports star",
+        "country": "Brazil"
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 290,
+        "description": "Model, influencer, fashion icon",
+        "country": "USA"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "follower_count": 250,
+        "description": "Singer, actress, performer",
+        "country": "USA"
+    },
+    {
+        "name": "Cardi B",
+        "follower_count": 160,
+        "description": "Rapper, influencer, entertainer",
+        "country": "USA"
+    },
+    {
+        "name": "David Beckham",
+        "follower_count": 100,
+        "description": "Footballer, businessman, style icon",
+        "country": "UK"
+    },
+    {
+        "name": "Chris Hemsworth",
+        "follower_count": 110,
+        "description": "Actor, Marvel's Thor star",
+        "country": "Australia"
+    },
+    {
+        "name": "Robert Downey Jr",
+        "follower_count": 55,
+        "description": "Actor, Marvel, global superstar",
+        "country": "USA"
+    },
+    {
+        "name": "Emma Watson",
+        "follower_count": 75,
+        "description": "Actress, activist, Harry Potter",
+        "country": "UK"
+    },
+    {
+        "name": "Gal Gadot",
+        "follower_count": 105,
+        "description": "Actress, Wonder Woman star",
+        "country": "Israel"
+    },
+    {
+        "name": "Johnny Depp",
+        "follower_count": 40,
+        "description": "Actor, Hollywood, film legend",
+        "country": "USA"
+    },
+    {
+        "name": "Priyanka Chopra",
+        "follower_count": 90,
+        "description": "Actress, singer, global star",
+        "country": "India"
+    },
+    {
+        "name": "Chris Evans",
+        "follower_count": 65,
+        "description": "Actor, Marvel, Captain America",
+        "country": "USA"
+    },
+    {
+        "name": "Will Smith",
+        "follower_count": 65,
+        "description": "Actor, rapper, Hollywood legend",
+        "country": "USA"
+    },
+    {
+        "name": "Miley Cyrus",
+        "follower_count": 215,
+        "description": "Singer, actress, pop culture",
+        "country": "USA"
+    },
+    {
+        "name": "Katy Perry",
+        "follower_count": 200,
+        "description": "Singer, songwriter, global star",
+        "country": "USA"
+    },
+    {
+        "name": "Ed Sheeran",
+        "follower_count": 130,
+        "description": "Singer, songwriter, global hits",
+        "country": "UK"
+    },
+    {
+        "name": "Shakira",
+        "follower_count": 120,
+        "description": "Singer, dancer, performer",
+        "country": "Colombia"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 280,
+        "description": "Singer, songwriter, pop star",
+        "country": "Canada"
+    },
+    {
+        "name": "Gigi Hadid",
+        "follower_count": 80,
+        "description": "Model, influencer, fashion star",
+        "country": "USA"
+    },
+    {
+        "name": "Zayn Malik",
+        "follower_count": 50,
+        "description": "Singer, songwriter, One Direction",
+        "country": "UK"
+    },
+    {
+        "name": "Harry Styles",
+        "follower_count": 140,
+        "description": "Singer, songwriter, pop idol",
+        "country": "UK"
+    },
+    {
+        "name": "Camila Cabello",
+        "follower_count": 70,
+        "description": "Singer, songwriter, Havana hit",
+        "country": "Cuba"
+    },
+    {
+        "name": "Dua Lipa",
+        "follower_count": 95,
+        "description": "Singer, pop star, performer",
+        "country": "UK"
+    },
+    {
+        "name": "Bad Bunny",
+        "follower_count": 125,
+        "description": "Singer, rapper, reggaeton star",
+        "country": "Puerto Rico"
+    },
+    {
+        "name": "Shawn Mendes",
+        "follower_count": 110,
+        "description": "Singer, songwriter, pop idol",
+        "country": "Canada"
+    },
+    {
+        "name": "Charlie Puth",
+        "follower_count": 55,
+        "description": "Singer, songwriter, piano pop",
+        "country": "USA"
+    },
+    {
+        "name": "Snoop Dogg",
+        "follower_count": 85,
+        "description": "Rapper, cultural icon, entertainer",
+        "country": "USA"
+    },
+    {
+        "name": "Post Malone",
+        "follower_count": 100,
+        "description": "Rapper, singer, pop culture",
+        "country": "USA"
+    },
+    {
+        "name": "Kourtney Kardashian",
+        "follower_count": 150,
+        "description": "Celebrity, influencer, entrepreneur",
+        "country": "USA"
+    }
+]
